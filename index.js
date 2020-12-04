93a7dc85746e7c9a8d64b408f8f6dec68f730646
s-rostami <49751278+s-rostami@users.noreply.github.com>
2020-12-04 15:31:07 -0500

    2nd commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send('this is the backend nodeserver to add city revirews to the dynamodb database.'));
+app.get('/', (req, res) => res.send('this is the backend nodeserver to add city revirews to the dynamodb database. V2V2V2V2'));
 
 app.post("/node_server", (req, res) => {
   const CityName = req.body.cityName;
